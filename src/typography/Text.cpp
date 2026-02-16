86498b27a0fb3374f77041026cb7617d948f8c42
Choo king <denshao2@gmail.com>
2026-02-16 16:44:51 -0500

    Use blit for rendering text.

--- src/typography/Text.cpp
+++ src/typography/Text.cpp
@@ -10,43 +10,49 @@ namespace squid {
     }
     void Text::draw() const {
         SDL_RenderTexture(renderer_, texture_, &src_rect_, &dst_rect_);
-        SDL_SetRenderDrawColor(renderer_, 255, 0, 0, 255);
-        const SDL_FRect diag_rect = {dst_rect_.x, dst_rect_.y, dst_rect_.w, dst_rect_.h};
-        SDL_RenderRect(renderer_, &diag_rect);
-        SDL_SetRenderDrawColor(renderer_, 0, 0, 0, 255);
     }
 
-    void Text::render_texture() {
+    void Text::measure_text() {
         const auto font_metrics = font_->font_metrics();
         const auto required_height =
                 font_metrics.height / 64;
         int required_width = 0;
+
         for (const auto c: text_) {
             const auto character = font_->get_character(c);
             required_width += (character->metrics.horiAdvance) / 64;
         }
+        required_width_ = required_width;
+        required_height_ = required_height;
+    }
+    void Text::render_texture() {
+        measure_text();
+        buffer_ = Buffer(required_width_, required_height_);
 
-        texture_ = SDL_CreateTexture(renderer_, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, required_width,
-                                     required_height);
+        const auto font_metrics = font_->font_metrics();
+
+        texture_ = SDL_CreateTexture(renderer_, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, required_width_,
+                                     required_height_);
         int x = 0;
         for (const auto c: text_) {
             const auto character = (font_->get_character(c)).get();
+            const auto buffer = character->buffer;
             const auto metrics = character->metrics;
-            if (character->pitch != 0) {
-                SDL_Rect dst_rect = {static_cast<int>(x + character->metrics.horiBearingX / 64),
-                                     static_cast<int>(font_metrics.ascender/64  - metrics.horiBearingY / 64),
-                                     (character->pitch / 4), static_cast<int>(character->rows)};
-
-                if (const auto error =
-                            SDL_UpdateTexture(texture_, &dst_rect, character->bitmap.get(), character->pitch);
-                    error != 0) {
-                    std::cerr << "Could not update texture: " << SDL_GetError() << std::endl;
-                }
+            if (buffer.get_width() != 0) {
+                buffer_.blit(character->buffer, static_cast<int>(x + character->metrics.horiBearingX / 64), static_cast<int>(font_metrics.ascender/64  - metrics.horiBearingY / 64));
             }
             x += (metrics.horiAdvance) / 64;
         }
-        src_rect_ = {0, 0, static_cast<float>(required_width), static_cast<float>(required_height)};
-        dst_rect_ = {static_cast<float>(x_), static_cast<float>(y_), static_cast<float>(required_width),
-                     static_cast<float>(required_height)};
+        src_rect_ = {0, 0, static_cast<float>(required_width_), static_cast<float>(required_height_)};
+        dst_rect_ = {static_cast<float>(x_), static_cast<float>(y_), static_cast<float>(required_width_),
+                     static_cast<float>(required_height_)};
+
+        const SDL_Rect dst_rect = {0, 0, static_cast<int>(required_width_), static_cast<int>(required_height_)};
+
+        if (const auto error = SDL_UpdateTexture(texture_, &dst_rect, buffer_.get_pixels().get(),
+                                           buffer_.get_width() * buffer_.get_bpp());
+            error != 0) {
+            std::cerr << "Could not update texture: " << SDL_GetError() << std::endl;
+        }
     }
 } // namespace squid
--- src/typography/Text.h
+++ src/typography/Text.h
@@ -2,7 +2,7 @@
 #define SQUID_TEXT_H
 #include <memory>
 #include <string>
-
+#include <SDL3/SDL.h>
 #include "Font.h"
 
 namespace squid {
@@ -11,11 +11,15 @@ namespace squid {
         std::shared_ptr<Font> font_;
         SDL_Texture* texture_ = nullptr;
         SDL_Renderer* renderer_ = nullptr;
+        Buffer buffer_;
         //Coordinates are actually offsets relative to the window or other element
         int x_ = 0;
         int y_ = 0;
         SDL_FRect src_rect_ = {0, 0, 0, 0};
         SDL_FRect dst_rect_ = {0, 0, 0, 0};
+        int required_width_ = 0;
+        int required_height_ = 0;
+        void measure_text();
         void render_texture();
     public:
         Text(std::wstring text, const std::shared_ptr<Font> &font, SDL_Renderer* renderer);
